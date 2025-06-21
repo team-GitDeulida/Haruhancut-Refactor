d8848b7adbf05867c0f01c9ef3aa01f7bdcfd84f
indextrown <indextrown@gmail.com>
2025-06-21 17:03:47 +0900

    chore: 🚀 1.0.1 릴리즈

--- Haruhancut/Source/App/SceneDelegate.swift
+++ Haruhancut/Source/App/SceneDelegate.swift
@@ -15,7 +15,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     var appCoordinator: AppCoordinator?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
